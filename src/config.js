973314356b3cb128fa4983d450703b6a71c6af7b
CelianB <celian-bastien@outlook.fr>
2020-08-26 14:09:11 +0200

    change repo article name

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const config = {
   githubUserName: "CelianB",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "react-blog-github",
+  githubRepo: "react-blog-github-articles",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
