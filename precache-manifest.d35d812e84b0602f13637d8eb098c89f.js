a1fa243a3857daccfa41893078e93c416a5e3519
CelianB <celian-bastien@outlook.fr>
2020-08-26 14:28:04 +0200

    Updates

---
+++ precache-manifest.d35d812e84b0602f13637d8eb098c89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e42d2a683863b2e4717f52472dfe5e7",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "30c01268a7f9778dfadd",
+    "url": "/react-blog-github/static/css/main.b1f5844a.chunk.css"
+  },
+  {
+    "revision": "cf352fcb46f8dc475556",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30c01268a7f9778dfadd",
+    "url": "/react-blog-github/static/js/main.188c1c5c.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
